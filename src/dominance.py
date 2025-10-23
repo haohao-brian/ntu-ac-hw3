91b269b0b6d64ce8c58a9c936968cb6f412b862a
haohao-brian <brian.chunhao@gmail.com>
2025-10-23 13:08:58 +0800

    test

--- src/dominance.py
+++ src/dominance.py
@@ -1,5 +1,7 @@
 from typing import Dict, Set
+
 from cfg import CFG, BasicBlock
+
 
 class DominatorTree:
     def __init__(self, cfg: CFG):
@@ -7,26 +9,12 @@ class DominatorTree:
         self.dom: Dict[BasicBlock, Set[BasicBlock]] = {}
         self.idom: Dict[BasicBlock, BasicBlock] = {}
         self.dom_frontiers: Dict[BasicBlock, Set[BasicBlock]] = {}
-        self.compute_dominators()
-        self.compute_dominance_frontiers()
 
-    def compute_dominators(self):
-        """
-        Computes the dominators for each basic block.
-        """
-        # TODO: Implement the iterative algorithm to compute dominators.
-        pass
+    def compute_dominators(self) -> None:
+        return
 
-    def compute_idom(self):
-        """
-        Computes the immediate dominator for each basic block.
-        """
-        # TODO: Compute immediate dominators based on the dominator sets.
-        pass
+    def compute_idom(self) -> None:
+        return
 
-    def compute_dominance_frontiers(self):
-        """
-        Computes the dominance frontiers for each basic block.
-        """
-        # TODO: Implement dominance frontier computation.
-        pass
+    def compute_dominance_frontiers(self) -> None:
+        return
